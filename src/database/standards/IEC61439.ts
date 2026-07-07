5c2861a32ee9b99466102dc28dfb0185e9cb1965
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 21:15:25 +0330

    Create IEC61439.ts

---
+++ src/database/standards/IEC61439.ts
@@ -0,0 +1,15 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+export const IEC61439={
+
+    maxTemperature:70,
+
+    maxBusbarTemperature:105,
+
+    frequency:50,
+
+    standardVoltage:400
+
+};
