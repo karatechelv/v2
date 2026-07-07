7be2827495e68a9f9a034fce7c67208e49a951b2
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 16:24:49 +0330

    Create ProjectButtons.tsx

---
+++ src/components/panel/ProjectButtons.tsx
@@ -0,0 +1,31 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+import "./ProjectButtons.css";
+
+function ProjectButtons(){
+
+    return(
+
+        <div className="projectButtons">
+
+            <button>
+
+                Save Project
+
+            </button>
+
+            <button>
+
+                Open Project
+
+            </button>
+
+        </div>
+
+    );
+
+}
+
+export default ProjectButtons;
