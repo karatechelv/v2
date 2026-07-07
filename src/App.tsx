6a70e48e12ebbaee2d4a75a7a13b778ff5b357df
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 14:22:38 +0330

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+/*****************************************************************
+ * Karatech ELV
+ * File : src/App.tsx
+ * Description :
+ * Main Application Component
+ *****************************************************************/
+
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+
+    return (
+
+        <>
+
+            <Dashboard />
+
+        </>
+
+    );
+
+}
+
+export default App;
