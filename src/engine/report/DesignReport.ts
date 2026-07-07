45c805a5af08a6db499fc49ae4b07cb97f212a72
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 21:48:16 +0330

    Create DesignReport.ts

---
+++ src/engine/report/DesignReport.ts
@@ -0,0 +1,21 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+export interface DesignReport{
+
+project:string;
+
+designer:string;
+
+date:string;
+
+breaker:string;
+
+cable:string;
+
+voltageDrop:number;
+
+remarks:string;
+
+}
