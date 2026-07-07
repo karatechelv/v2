fc98195aa05e79e4e9e6311adaff9be070aa813a
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 16:17:13 +0330

    Create StatusBar.tsx

---
+++ src/components/panel/StatusBar.tsx
@@ -0,0 +1,21 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+import "./StatusBar.css";
+
+function StatusBar(){
+
+    return(
+
+        <div className="statusBar">
+
+            Ready
+
+        </div>
+
+    );
+
+}
+
+export default StatusBar;
