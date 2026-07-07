9cc42323c7f3bf4d24e513044967e8767b57d80e
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 16:16:13 +0330

    Create SelectionBox.tsx

---
+++ src/components/panel/SelectionBox.tsx
@@ -0,0 +1,17 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+import "./SelectionBox.css";
+
+function SelectionBox(){
+
+    return(
+
+        <div className="selectionBox"/>
+
+    );
+
+}
+
+export default SelectionBox;
