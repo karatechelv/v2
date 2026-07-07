0344e4564c78cf756421e6bd2de26a466f92a213
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 16:28:42 +0330

    Create WireCanvas.tsx

---
+++ src/components/wiring/WireCanvas.tsx
@@ -0,0 +1,19 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+import "./WireCanvas.css";
+
+function WireCanvas(){
+
+    return(
+
+        <svg className="wireCanvas">
+
+        </svg>
+
+    );
+
+}
+
+export default WireCanvas;
