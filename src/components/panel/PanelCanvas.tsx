107cd0bda6bd73a037bb85fd7367cb5a72b3129c
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 15:37:03 +0330

    Create PanelCanvas.tsx

---
+++ src/components/panel/PanelCanvas.tsx
@@ -0,0 +1,28 @@
+/*****************************************************************
+ * Karatech ELV
+ * File : src/components/panel/PanelCanvas.tsx
+ * Description :
+ * Panel Drawing Area
+ *****************************************************************/
+
+import "./PanelCanvas.css";
+
+function PanelCanvas() {
+
+    return (
+
+        <div className="panelCanvas">
+
+            <div className="panelGrid">
+
+                Workspace
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default PanelCanvas;
