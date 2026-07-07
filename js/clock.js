ed7843108b7bcaf9f9f313e437de186330669ef3
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 09:59:07 +0330

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,19 +1,33 @@
+/*========================================
+ KARATECH ELV
+ Clock System
+========================================*/
+
+const clockElement = document.getElementById("clock");
+
 function updateClock() {
 
     const now = new Date();
 
-    const h = String(now.getHours()).padStart(2, "0");
-    const m = String(now.getMinutes()).padStart(2, "0");
-    const s = String(now.getSeconds()).padStart(2, "0");
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
 
-    const clock = document.getElementById("clock");
+    hour = String(hour).padStart(2, "0");
+    minute = String(minute).padStart(2, "0");
+    second = String(second).padStart(2, "0");
 
-    if (clock) {
-        clock.innerHTML = h + ":" + m + ":" + s;
+    if (clockElement) {
+
+        clockElement.innerHTML =
+            hour + ":" +
+            minute + ":" +
+            second;
+
     }
 
 }
 
-setInterval(updateClock,1000);
-
 updateClock();
+
+setInterval(updateClock,1000);
