ed850816a74ff7eb967ab545f2fb679c2ef7e5ab
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 15:40:01 +0330

    Create Properties.tsx

---
+++ src/components/panel/Properties.tsx
@@ -0,0 +1,23 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+import "./Properties.css";
+
+function Properties(){
+
+    return(
+
+        <div className="propertiesPanel">
+
+            <h2>Properties</h2>
+
+            <p>No Component Selected</p>
+
+        </div>
+
+    )
+
+}
+
+export default Properties;
