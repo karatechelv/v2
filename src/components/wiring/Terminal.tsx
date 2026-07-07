70a56d6f059aa192e4e8227fdd987cfd0a15c96a
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 16:30:44 +0330

    Create Terminal.tsx

---
+++ src/components/wiring/Terminal.tsx
@@ -0,0 +1,21 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+import "./Terminal.css";
+
+function Terminal(){
+
+    return(
+
+        <div className="terminal">
+
+            X1
+
+        </div>
+
+    );
+
+}
+
+export default Terminal;
