49ee2018af802535c61c6b9dbe0de8bf8acc40d7
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 16:28:12 +0330

    Create Wire.ts

---
+++ src/types/Wire.ts
@@ -0,0 +1,20 @@
+/*****************************************************************
+ * Karatech ELV
+ * Wire Model
+ *****************************************************************/
+
+export interface Wire{
+
+    id:number;
+
+    from:number;
+
+    to:number;
+
+    color:string;
+
+    size:number;
+
+    label:string;
+
+}
