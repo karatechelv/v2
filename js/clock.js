c9e4b191ec755edc62d6fa3ffb69c9cb5310f66c
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 09:30:03 +0330

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+function updateClock() {
+
+    const now = new Date();
+
+    const h = String(now.getHours()).padStart(2, "0");
+    const m = String(now.getMinutes()).padStart(2, "0");
+    const s = String(now.getSeconds()).padStart(2, "0");
+
+    const clock = document.getElementById("clock");
+
+    if (clock) {
+        clock.innerHTML = h + ":" + m + ":" + s;
+    }
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
