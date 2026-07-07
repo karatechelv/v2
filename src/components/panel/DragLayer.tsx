a152367ac83f191b68ebacfc20cee5f1da0b2559
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 16:10:16 +0330

    Create DragLayer.tsx

---
+++ src/components/panel/DragLayer.tsx
@@ -0,0 +1,20 @@
+/*****************************************************************
+ * Karatech ELV
+ * Drag Layer
+ *****************************************************************/
+
+import "./DragLayer.css";
+
+function DragLayer(){
+
+    return(
+
+        <div className="dragLayer">
+
+        </div>
+
+    );
+
+}
+
+export default DragLayer;
