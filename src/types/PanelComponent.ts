aec72b844ae00d44aa46e6479b25680b5e9de907
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 15:44:10 +0330

    Create PanelComponent.ts

---
+++ src/types/PanelComponent.ts
@@ -0,0 +1,16 @@
+/*****************************************************************
+ * Karatech ELV
+ * File : PanelComponent.ts
+ *****************************************************************/
+
+export interface PanelComponent{
+
+    id:number;
+
+    name:string;
+
+    x:number;
+
+    y:number;
+
+}
