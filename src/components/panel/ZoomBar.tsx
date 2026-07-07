8abfa2f9b806a55fb53e588a13f397dafa070ddc
karatechelv <keramati.m.pcp@gmail.com>
2026-07-07 16:25:47 +0330

    Create ZoomBar.tsx

---
+++ src/components/panel/ZoomBar.tsx
@@ -0,0 +1,25 @@
+/*****************************************************************
+ * Karatech ELV
+ *****************************************************************/
+
+import "./ZoomBar.css";
+
+function ZoomBar(){
+
+    return(
+
+        <div className="zoomBar">
+
+            <button>＋</button>
+
+            <button>－</button>
+
+            <button>100%</button>
+
+        </div>
+
+    );
+
+}
+
+export default ZoomBar;
