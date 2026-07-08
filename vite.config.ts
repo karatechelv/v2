d4ea974a9ba9100031fde6c70eb30f8d48667434
karatechelv <keramati.m.pcp@gmail.com>
2026-07-08 08:00:36 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,68 @@
+/****************************************************************************************
+ * Karatech ELV
+ * File : vite.config.ts
+ * Description :
+ * Vite Configuration
+ ****************************************************************************************/
+
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
 
-  server: {
-    host: true,
-    port: 5173,
-    open: true
-  },
+    base:"/v2/",
 
-  preview: {
-    port: 4173
-  },
+    plugins:[
 
-  resolve: {
-    alias: {
-      "@": "/src",
-      "@components": "/src/components",
-      "@pages": "/src/pages",
-      "@layouts": "/src/layouts",
-      "@router": "/src/router",
-      "@hooks": "/src/hooks",
-      "@services": "/src/services",
-      "@store": "/src/store",
-      "@styles": "/src/styles",
-      "@assets": "/src/assets",
-      "@database": "/src/database"
+        react()
+
+    ],
+
+    server:{
+
+        host:true,
+
+        port:5173,
+
+        open:true
+
+    },
+
+    preview:{
+
+        port:4173
+
+    },
+
+    resolve:{
+
+        alias:{
+
+            "@":"/src",
+
+            "@components":"/src/components",
+
+            "@pages":"/src/pages",
+
+            "@layouts":"/src/layouts",
+
+            "@router":"/src/router",
+
+            "@hooks":"/src/hooks",
+
+            "@services":"/src/services",
+
+            "@store":"/src/store",
+
+            "@styles":"/src/styles",
+
+            "@assets":"/src/assets",
+
+            "@database":"/src/database"
+
+        }
+
     }
-  }
+
 });
