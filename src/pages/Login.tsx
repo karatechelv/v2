95d2f079b8eb1693ba704ff05a893ab0e7daa467
karatechelv <keramati.m.pcp@gmail.com>
2026-07-15 23:45:22 +0330

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,63 +1,10 @@
-import "./Login.css";
-
-function Login() {
-  return (
-    <div className="login">
-
-      <div className="login__overlay">
-
-        <div className="login__card">
-
-          <img
-            src="/logo.png"
-            alt="KARATECH ELV"
-            className="login__logo"
-          />
-
-          <h1>KARATECH ELV</h1>
-
-          <h2>Engineering Operating System</h2>
-
-          <p className="login__description">
-            Electrical • Automation • ELV • Industrial
-          </p>
-
-          <input
-            type="text"
-            placeholder="Username"
-          />
-
-          <input
-            type="password"
-            placeholder="Password"
-          />
-
-          <button>
-            SIGN IN
-          </button>
-
-          <div className="login__footer">
-
-            <p>
-              Designed, Developed & Engineered by
-            </p>
-
-            <strong>
-              Mohsen Keramati Layegh
-            </strong>
-
-            <span>
-              Version 1.0.0
-            </span>
-
-          </div>
-
-        </div>
-
-      </div>
-
-    </div>
-  );
-}
-
-export default Login;
+<img
+  src="/logo.png"
+  alt="KARATECH ELV"
+  style={{
+    width: "300px",
+    height: "300px",
+    border: "4px solid red",
+    background: "white"
+  }}
+/>
