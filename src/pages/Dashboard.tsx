a52823bf872be16db809a41fd0e70d647b1c2c46
karatechelv <keramati.m.pcp@gmail.com>
2026-07-15 16:16:40 +0330

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,14 @@
-return (
-  <>
-    <Header />
-    <div style={{ color: "white", padding: "40px" }}>
-      Header OK
-    </div>
-  </>
-);
+import Header from "../components/layout/Header";
+
+function Dashboard() {
+  return (
+    <>
+      <Header />
+      <div style={{ color: "white", padding: "40px" }}>
+        Header OK
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
