55ec9cb217b7e0cd68957406220567f9c0a00c69
karatechelv <keramati.m.pcp@gmail.com>
2026-07-18 23:15:14 +0330

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
-/*****************************************************************
+/******************************************************************************
  * Karatech ELV
  * File : src/components/layout/Header.tsx
- *****************************************************************/
+******************************************************************************/
 
 import "./Header.css";
 
@@ -9,37 +9,69 @@ import Clock from "./Clock";
 import DateBox from "./DateBox";
 import UserSessionTimer from "./UserSessionTimer";
 
-function Header(){
+function Header() {
 
-    return(
+    return (
 
         <header className="header">
 
+            {/* LEFT */}
+
             <div className="header__left">
 
+                <div className="header__brand">
+
+                    <img
+                        src="/v2/logo.png"
+                        alt="Karatech ELV"
+                        className="header__logo"
+                    />
+
+                    <div>
+
+                        <h1>KARATECH <span>ELV</span></h1>
+
+                        <p>Engineering Operating System</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            {/* CENTER */}
+
+            <div className="header__center">
+
                 <input
-
                     className="header__search"
-
                     type="text"
-
-                    placeholder="جستجو در کل سامانه..."
-
+                    placeholder="Search... | جستجو در سامانه"
                 />
 
             </div>
 
-            <div className="header__center">
-
-                <h2 className="header__title">
-
-                    KARATECH ELV
-
-                </h2>
-
-            </div>
+            {/* RIGHT */}
 
             <div className="header__right">
+
+                <button className="header__lang">
+
+                    FA | EN
+
+                </button>
+
+                <button className="header__icon">
+
+                    ؟
+
+                </button>
+
+                <button className="header__icon">
+
+                    ☎
+
+                </button>
 
                 <DateBox />
 
